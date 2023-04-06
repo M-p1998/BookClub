1c13d0c13fb4c5690cb6c09bc1bce726e248d18c
M_p <skyusmpp@gmail.com>
2023-04-05 22:35:50 -0700

    updates

--- books_project/settings.py
+++ books_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('1m($@57v(b$7%%!9hdvm^4zzbuv!_&*-84m_hjb@@()t!e__9p')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
